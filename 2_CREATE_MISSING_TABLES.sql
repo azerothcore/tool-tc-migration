20f762647e9bb33843b7e7fed1d004d23d924918
Miorey <clem.balea@gmail.com>
2022-11-01 01:18:34 +0200

    Add / remove fields Add tables Fix tables

--- 2_CREATE_MISSING_TABLES.sql
+++ 2_CREATE_MISSING_TABLES.sql
@@ -64,15 +64,19 @@ CREATE TABLE `gm_subsurveys` (
   PRIMARY KEY (`surveyId`,`subsurveyId`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='Player System';
 
-CREATE TABLE `gm_surveys` (
-  `surveyId` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `guid` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  `mainSurvey` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  `overallComment` LONGTEXT NOT NULL,
-  `createTime` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  `maxMMR` SMALLINT(5) NOT NULL,
+--
+-- gm_surveys AC commit: e0d36be56ec2b7c7c88c7b86b81512106cef535e
+--
+DROP TABLE IF EXISTS `gm_survey`;
+CREATE TABLE IF NOT EXISTS `gm_survey` (
+  `surveyId` int unsigned NOT NULL AUTO_INCREMENT,
+  `guid` int unsigned NOT NULL DEFAULT '0',
+  `mainSurvey` int unsigned NOT NULL DEFAULT '0',
+  `comment` longtext NOT NULL,
+  `createTime` int unsigned NOT NULL DEFAULT '0',
+  `maxMMR` smallint NOT NULL,
   PRIMARY KEY (`surveyId`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='Player System';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Player System';
 
 CREATE TABLE `gm_tickets` (
   `ticketId` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -133,6 +137,8 @@ CREATE TABLE `channels_bans` (
   PRIMARY KEY (`channelId`,`playerGUID`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
+
+
 --
 -- Table structure for table `channels_rights`
 --
@@ -161,70 +167,115 @@ LOCK TABLES `channels_rights` WRITE;
 /*!40000 ALTER TABLE `channels_rights` ENABLE KEYS */;
 UNLOCK TABLES;
 
----
---- character_settings
----
-create table character_settings
+--
+-- character_settings AC commit: d4963f3b83c8a327904addb129d5fd5a15b30d25
+--
+DROP TABLE IF EXISTS `character_settings`;
+CREATE TABLE `character_settings` (
+  `guid` INT UNSIGNED NOT NULL,
+  `source` VARCHAR(40) NOT NULL,
+  `data` TEXT NULL,
+  PRIMARY KEY (`guid`, `source`)
+) ENGINE=MYISAM DEFAULT CHARSET=utf8mb4 COMMENT='Player Settings';
+-- TODO Why ENGINE is MYISAM and not InnoDB like the others ?
+
+--
+-- game_event_condition_save AC commit: fc05c82f659692b4c79c85aec1548f6aa0c1d537
+--
+DROP TABLE IF EXISTS `game_event_condition_save`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_event_condition_save`
 (
-	guid int(10) unsigned not null,
-	source varchar(40) not null,
-	data text null,
-	primary key (guid, source)
-)
- ENGINE=INNODB DEFAULT CHARSET=utf8 comment 'Player Settings';
+  `eventEntry` tinyint(3) unsigned NOT NULL,
+  `condition_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `done` float DEFAULT '0',
+  PRIMARY KEY (`eventEntry`,`condition_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
----
---- game_event_condition_save
----
-create table if not exists game_event_condition_save
+
+--
+-- creature_respawn AC commit: fc05c82f659692b4c79c85aec1548f6aa0c1d537
+--
+DROP TABLE IF EXISTS `creature_respawn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `creature_respawn`
 (
-	eventEntry tinyint(3) unsigned not null,
-	condition_id int(10) unsigned default 0 not null,
-	done float default 0 null,
-	primary key (eventEntry, condition_id)
-);
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+  `respawnTime` int(10) unsigned NOT NULL DEFAULT '0',
+  `mapId` smallint(10) unsigned NOT NULL DEFAULT '0',
+  `instanceId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Instance Identifier',
+  PRIMARY KEY (`guid`,`instanceId`),
+  KEY `idx_instance` (`instanceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Grid Loading System';
 
-
----
---- creature_respawn
----
-create table creature_respawn
+--
+-- gameobject_respawn AC commit: fc05c82f659692b4c79c85aec1548f6aa0c1d537
+--
+DROP TABLE IF EXISTS `gameobject_respawn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gameobject_respawn`
 (
-	guid int(10) unsigned default 0 not null comment 'Global Unique Identifier',
-	respawnTime int(10) unsigned default 0 not null,
-	mapId smallint(5) unsigned default 0 not null,
-	instanceId int(10) unsigned default 0 not null comment 'Instance Identifier',
-	primary key (guid, instanceId)
-)
- ENGINE=INNODB DEFAULT CHARSET=utf8 comment 'Grid Loading System';
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+  `respawnTime` int(10) unsigned NOT NULL DEFAULT '0',
+  `mapId` smallint(10) unsigned NOT NULL DEFAULT '0',
+  `instanceId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Instance Identifier',
+  PRIMARY KEY (`guid`,`instanceId`),
+  KEY `idx_instance` (`instanceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Grid Loading System';
 
-create index idx_instance
-	on creature_respawn (instanceId);
+--
+-- instance_saved_go_state_data AC commit: a6a2ca8ef76716f5b58b4ed2ae53d0bde424775b
+--
+DROP TABLE IF EXISTS `instance_saved_go_state_data`;
+CREATE TABLE IF NOT EXISTS `instance_saved_go_state_data` (
+  `id` INT UNSIGNED NOT NULL COMMENT 'instance.id',
+  `guid` INT UNSIGNED NOT NULL COMMENT 'gameobject.guid',
+  `state` TINYINT UNSIGNED DEFAULT '0' COMMENT 'gameobject.state',
+  PRIMARY KEY (`id`, `guid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
----
---- gameobject_respawn
----
-create table gameobject_respawn
-(
-	guid int(10) unsigned default 0 not null comment 'Global Unique Identifier',
-	respawnTime int(10) unsigned default 0 not null,
-	mapId smallint(5) unsigned default 0 not null,
-	instanceId int(10) unsigned default 0 not null comment 'Instance Identifier',
-	primary key (guid, instanceId)
-)
- ENGINE=INNODB DEFAULT CHARSET=utf8 comment 'Grid Loading System';
+--
+-- mail_server_character AC commit: 7ecd73867419c807c9869ebeb73c7ca4e39cd1c8
+--
+DROP TABLE IF EXISTS `mail_server_character`;
+CREATE TABLE IF NOT EXISTS `mail_server_character` (
+  `guid` INT UNSIGNED NOT NULL,
+  `mailId` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`guid`, `mailId`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
 
-create index idx_instance
-	on gameobject_respawn (instanceId);
+--
+-- mail_server_template AC commit: 7ecd73867419c807c9869ebeb73c7ca4e39cd1c8
+--
+DROP TABLE IF EXISTS `mail_server_template`;
+CREATE TABLE IF NOT EXISTS `mail_server_template` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `reqLevel` TINYINT unsigned NOT NULL DEFAULT 0,
+  `reqPlayTime` INT unsigned NOT NULL DEFAULT 0,
+  `moneyA` INT unsigned NOT NULL DEFAULT 0,
+  `moneyH` INT unsigned NOT NULL DEFAULT 0,
+  `itemA` INT unsigned NOT NULL DEFAULT 0,
+  `itemCountA` INT unsigned NOT NULL DEFAULT 0,
+  `itemH` INT unsigned NOT NULL DEFAULT 0,
+  `itemCountH` INT unsigned NOT NULL DEFAULT 0,
+  `subject` text NOT NULL,
+  `body` text NOT NULL,
+  `active` TINYINT unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
----
---- instance_saved_go_state_data
----
-create table instance_saved_go_state_data
-(
-	id int(10) unsigned not null comment 'instance.id',
-	guid int(10) unsigned not null comment 'gameobject.guid',
-	state tinyint(3) unsigned default 0 null comment 'gameobject.state',
-	primary key (id, guid)
-);
-
+--
+-- recovery_item AC commit: 333dab2e8515e6d8ee3b721c042f5576fde9be2d
+--
+CREATE TABLE IF NOT EXISTS `recovery_item` (
+  `Id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Guid` int(11) unsigned NOT NULL DEFAULT 0,
+  `ItemEntry` mediumint(8) unsigned NOT NULL DEFAULT 0,
+  `Count` int(11) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`Id`),
+  KEY `idx_guid` (`Guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- 3_ALTER_TABLES.sql
+++ 3_ALTER_TABLES.sql
@@ -1,5 +1,13 @@
--- character_spell_cooldown
-ALTER TABLE `character_spell_cooldown` ADD COLUMN `needSend` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' AFTER `categoryEnd`;
+-- character_spell_cooldown / pet_spell_cooldown AC commit: bd956b5a5750014bd4168dd82df3438dcc5df43f
+ALTER TABLE `character_spell_cooldown` ADD COLUMN `category` MEDIUMINT UNSIGNED DEFAULT 0 NOT NULL AFTER `spell`;
+ALTER TABLE `pet_spell_cooldown` ADD COLUMN `category` MEDIUMINT UNSIGNED DEFAULT 0 NOT NULL AFTER `spell`;
+
+ALTER TABLE `character_spell_cooldown` ADD COLUMN `needSend` tinyint(3) unsigned DEFAULT 1 NOT NULL AFTER `time`;
+
+ALTER TABLE `character_spell_cooldown` DROP COLUMN `categoryEnd`;
+ALTER TABLE `character_spell_cooldown` DROP COLUMN `categoryId`;
+ALTER TABLE `pet_spell_cooldown` DROP COLUMN `categoryEnd`;
+ALTER TABLE `pet_spell_cooldown` DROP COLUMN `categoryId`;
 
 -- channels
 ALTER TABLE `channels` 
@@ -33,3 +41,13 @@ ALTER TABLE character_spell ADD `specMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT 
 
 -- characters
 ALTER TABLE characters ADD `order` tinyint(4) NULL AFTER grantableLevels;
+ALTER TABLE `characters` ADD COLUMN `creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `grantableLevels`;
+
+-- character_homebind AC commit: 2e6f6e26da8f01cd67bbb56e0e7aa961ffd5f51f
+ALTER TABLE `character_homebind` ADD COLUMN `posO` FLOAT NOT NULL DEFAULT '0' AFTER `posZ`;
+
+-- log_money AC commit: 2fec54c4429a03b406b30bbce29c5b376ad04e31
+ALTER TABLE `log_money` ADD COLUMN `type` TINYINT NOT NULL COMMENT '1=COD,2=AH,3=GB DEPOSIT,4=GB WITHDRAW,5=MAIL,6=TRADE' AFTER `date`;
+
+--
+ALTER TABLE auctionhouse DROP COLUMN `Flags`;
