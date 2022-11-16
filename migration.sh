77217a80f01ba460d8ad8c96bcbafa96737b4c64
Miorey <clem.balea@gmail.com>
2022-11-17 01:06:21 +0200

    improve migration script for multi characters DBs. add doc

--- migration.sh
+++ migration.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
 ############################################################
 # Help                                                     #
 ############################################################
+# Source progress bar
+MIGRATION_CHARACTERS_FILE=('1_CREATE_CLEANUP_TABLES' '2_CREATE_MISSING_TABLES' '3_ALTER_TABLES' '4_CLEANUP_AND_CONVERT_SPELLS' '5_FINAL_CLEANUP')
+MIGRATION_AUTH_FILE=('6.AUTH_RENAME_COLUMNS' '7.AUTH_CLEAN_AND_CONVERTER')
+
+
+IFS=':' read -r -a CHARACTERS_DBS_ARRAY <<< "$CHARACTERS_DBS"
+IFS=':' read -r -a DUMP_CHARACTERS_FILES_PATH_ARRAY <<< "$DUMP_CHARACTERS_FILES_PATH"
+
+RepeatChar(){
+	for _ in {1..90}; do echo -n "$1"; done
+	echo
+}
+
 Help()
 {
    # Display Help
@@ -17,39 +31,72 @@ Help()
 
 Restore()
 {
-  /usr/bin/mysql --database="${CHARACTERS_DB}" \
+  RepeatChar '-'
+  echo "Restore ${AUTH_DB}"
+  RepeatChar '-'
+  /usr/bin/mysql\
   --user="${DB_USER}" \
   --password="${DB_PASS}" \
   --host="${DB_HOSTNAME}" \
   --port="${DB_PORT}" \
   -e "
-  DROP DATABASE ${CHARACTERS_DB};
-  CREATE DATABASE  ${CHARACTERS_DB};
-  DROP DATABASE ${AUTH_DB};
+  DROP DATABASE IF EXISTS ${AUTH_DB};
   CREATE DATABASE  ${AUTH_DB};
   "
 
-  /usr/bin/mysql --database="${CHARACTERS_DB}" --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOSTNAME}" --port="${DB_PORT}" < "${DUMP_CHARACTERS_FILE_PATH}"
   /usr/bin/mysql --database="${AUTH_DB}" --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOSTNAME}" --port="${DB_PORT}" < "${DUMP_AUTH_FILE_PATH}"
+
+  for db_index in "${!CHARACTERS_DBS_ARRAY[@]}"; do
+    RepeatChar '-'
+    echo "Restore ${CHARACTERS_DBS_ARRAY[$db_index]}"
+    RepeatChar '-'
+
+    /usr/bin/mysql\
+    --user="${DB_USER}" \
+    --password="${DB_PASS}" \
+    --host="${DB_HOSTNAME}" \
+    --port="${DB_PORT}" \
+    -e "
+    DROP DATABASE IF EXISTS ${CHARACTERS_DBS_ARRAY[$db_index]};
+    CREATE DATABASE  ${CHARACTERS_DBS_ARRAY[$db_index]};
+    "
+    /usr/bin/mysql --database="${CHARACTERS_DBS_ARRAY[$db_index]}"\
+    --user="${DB_USER}"\
+    --password="${DB_PASS}"\
+    --host="${DB_HOSTNAME}"\
+    --port="${DB_PORT}"\
+    < "${DUMP_CHARACTERS_FILES_PATH_ARRAY[$db_index]}"
+  done
 }
 
 MigrateAuth()
 {
-  MIGRATION_FILE=('1_CREATE_CLEANUP_TABLES' '2_CREATE_MISSING_TABLES' '3_ALTER_TABLES' '4_CLEANUP_AND_CONVERT_SPELLS' '5_FINAL_CLEANUP')
-  for file_name in "${MIGRATION_FILE[@]}"
+  RepeatChar '-'
+  echo "Migrate ${AUTH_DB}"
+  RepeatChar '-'
+  for file_name in "${MIGRATION_AUTH_FILE[@]}"
   do
     echo "Execute: $file_name"
-    /usr/bin/mysql --database="${CHARACTERS_DB}" --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOSTNAME}" --port="${DB_PORT}" < ./"${file_name}".sql
+    /usr/bin/mysql --database="${AUTH_DB}" --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOSTNAME}" --port="${DB_PORT}" < ./"${file_name}".sql
   done
 }
 
 MigrateCharacters()
 {
-  MIGRATION_FILE=('6.AUTH_RENAME_COLUMNS' '7.AUTH_CLEAN_AND_CONVERTER')
-  for file_name in "${MIGRATION_FILE[@]}"
-  do
-    echo "Execute: $file_name"
-    /usr/bin/mysql --database="${AUTH_DB}" --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOSTNAME}" --port="${DB_PORT}" < ./"${file_name}".sql
+  for db_index in "${!CHARACTERS_DBS_ARRAY[@]}"; do
+    RepeatChar '-'
+    echo "Migrate ${CHARACTERS_DBS_ARRAY[$db_index]}"
+    RepeatChar '-'
+    for file_name in "${MIGRATION_CHARACTERS_FILE[@]}"
+    do
+      echo "Execute: $file_name"
+      /usr/bin/mysql --database="${CHARACTERS_DBS_ARRAY[$db_index]}"\
+      --user="${DB_USER}"\
+      --password="${DB_PASS}"\
+      --host="${DB_HOSTNAME}"\
+      --port="${DB_PORT}"\
+      < "${file_name}.sql"
+    done
   done
 }
 
@@ -57,7 +104,7 @@ MigrateCharacters()
 # Main program                                             #
 ############################################################
 # Get the options
-while getopts ":hrmac" option; do
+while getopts ":hrma" option; do
    case $option in
       h) # display Help
          Help
@@ -66,17 +113,13 @@ while getopts ":hrmac" option; do
         Restore
         exit;;
       m) # Enter a name
-        MigrateAuth
         MigrateCharacters
+        MigrateAuth
         exit;;
       a) # Enter a name
         Restore
+        MigrateCharacters
         MigrateAuth
-        MigrateCharacters
-        exit;;
-      c) # Enter a name
-        Restore
-        MigrateCharacters
         exit;;
      \?) # Invalid option
          Help
